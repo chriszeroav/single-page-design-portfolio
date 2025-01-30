401be505a934c7199d12e191c1764238405a033c
Christopher Romero <chris.zeroav@gmail.com>
2025-01-29 20:28:38 -0500

    base structure for init this project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "custom-dark": "#030303",
+        "custom-medium-brown": "#7A746E",
+        "custom-light-cream": "#FFF7F0",
+        "custom-galactic-blue": "#755CDE",
+        "custom-summer-yellow": "#F6A560",
+        "custom-pink": "#F39E9E",
+        "custom-light-red": "#EB7565",
+        "custom-cyan": "#61C4B7",
+        "custom-dark-purple": "#552049",
+      },
+      fontFamily: {
+        sans: ["var(--font-plus-jakarta-sans)"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
